39fbd30027d278cd77f6675065a9ca4f3cf049e0
Oskar Stark <oskarstark@googlemail.com>
2020-12-08 08:51:56 +0100

    [AllBundles] Enhancement: Streamline tests (#2827)
    * [AllBundles] Enhancement: Streamline tests
    
    * Enhancement: Fix namespace
    
    * Update src/Kunstmaan/TranslatorBundle/Tests/app/AppKernel.php
    
    Co-authored-by: Jeroen Thora <jeroenthora@gmail.com>
    
    * Update src/Kunstmaan/TranslatorBundle/Tests/app/AppKernel.php
    
    Co-authored-by: Jeroen Thora <jeroenthora@gmail.com>
    
    * cs fix
    
    * cs fix
    
    * Fix tests
    
    * Codestyle fixes
    
    Co-authored-by: Jeroen Thora <jeroenthora@gmail.com>

--- Tests/Helper/HostOverrideCleanupHandlerTest.php
+++ Tests/Helper/HostOverrideCleanupHandlerTest.php
@@ -17,10 +17,6 @@ class HostOverrideCleanupHandlerTest extends TestCase
      */
     protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
     protected function setUp()
     {
         $this->object = new HostOverrideCleanupHandler();
